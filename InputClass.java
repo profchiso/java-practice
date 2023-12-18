9d5807e53b08ca822cd2d3f26bc66192dbc62acf
Chinedu Okorie <okoriechinedusunday@gmail.com>
2023-12-18 11:23:22 +0100

    Feat: work mor on input

--- InputClass.java
+++ InputClass.java
@@ -6,7 +6,9 @@ public class InputClass {
     Scanner scan = new Scanner(System.in);
     System.out.println("Entered a name  ");
     String myName = scan.nextLine();
-    System.out.println("Entered name is  " + myName);
+    System.out.println("Entered an Age  ");
+    int age = scan.nextInt();
+    System.out.println("Entered name is  " + myName + " entered age is " + age);
     scan.close();
   }
 }
