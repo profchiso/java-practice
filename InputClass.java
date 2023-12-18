fc184021f65d9cfe85bcc253b9185de236a32704
Chinedu Okorie <okoriechinedusunday@gmail.com>
2023-12-18 11:27:51 +0100

    work with big decimal

--- InputClass.java
+++ InputClass.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class InputClass {
@@ -8,6 +9,11 @@ public class InputClass {
     String myName = scan.nextLine();
     System.out.println("Entered an Age  ");
     int age = scan.nextInt();
+    BigDecimal d = new BigDecimal(30);
+    for (int i = 0; i < age; i++) {
+      d = d.add(new BigDecimal("1.1"));
+      System.out.println(d);
+    }
     System.out.println("Entered name is  " + myName + " entered age is " + age);
     scan.close();
   }
